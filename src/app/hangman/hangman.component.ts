76a0158a1cae1bfc8701bc683b835e9be73d4852
shermana <shermana@holos.cl>
2023-06-19 21:28:26 -0300

    create components

---
+++ src/app/hangman/hangman.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hangman',
+  templateUrl: './hangman.component.html',
+  styleUrls: ['./hangman.component.css']
+})
+export class HangmanComponent {
+
+}
