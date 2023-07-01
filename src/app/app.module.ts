9b591d73ac29d691874c5d309cebf5585e5731dd
shermana <shermana@holos.cl>
2023-07-01 13:19:02 -0300

    create components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HangmanComponent } from './hangman/hangman.component';
-import { InputComponent } from './input/input.component';
-import { StatusComponent } from './status/status.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HangmanComponent } from './components/hangman/hangman.component';
+import { InputComponent } from './components/input/input.component';
+import { StatusComponent } from './components/status/status.component';
 
 @NgModule({
   declarations: [
