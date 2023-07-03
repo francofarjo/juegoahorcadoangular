ea5417dffcff65872920882f7b352ae0d4903f2a
franco farjo <francofarjo@hotmail.com>
2023-07-03 00:06:43 -0300

    general

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
     production: true,
-    firebaseConfig: {
-      apiKey: "AIzaSyBGv-vRNuka4Qv8RR1J9qBIPxy7QaPo55g",
-      authDomain: "ahorcado-d8fa0.firebaseapp.com",
-      projectId: "ahorcado-d8fa0",
-      storageBucket: "ahorcado-d8fa0.appspot.com",
-      messagingSenderId: "410527447701",
-      appId: "1:410527447701:web:0d88b9c596352ffe27399c",
-      measurementId: "G-Y6YLPZ487W",
+    firebaseConfig:{
+      apiKey: 'AIzaSyDubx48xJvddn5iXTDEXidp_5COJWAY_-Q',
+      authDomain: 'ahorcado-e4bb1.firebaseapp.com',
+      databaseURL: 'https://ahorcado-e4bb1-default-rtdb.firebaseio.com',
+      projectId: 'ahorcado-e4bb1',
+      storageBucket: 'ahorcado-e4bb1.appspot.com',
+      messagingSenderId: '639803648769',
+      appId: '1:639803648769:web:6eac1e2565120b0b91c900',
+      measurementId: 'G-TC3ZEM778W'
     },
   };
   
